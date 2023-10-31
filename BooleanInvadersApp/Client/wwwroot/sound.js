4043b91871c980215262f5cdae33fe1c9926e1aa
Ävadakedavr0 <dylandd4@hotmail.be>
2023-10-31 11:26:52 +0100

    Game functionallity done - now add api for calling db for users and highscores

---
+++ BooleanInvadersApp/Client/wwwroot/sound.js
@@ -0,0 +1,29 @@
+﻿function playAudio(elementId) {
+    var audio = document.getElementById(elementId);
+    audio.play();
+}
+
+function pauseAudio(elementId) {
+    var audio = document.getElementById(elementId);
+    audio.pause();
+}
+
+function muteAudio(elementId) {
+    var audio = document.getElementById(elementId);
+    audio.muted = true;
+}
+
+function unmuteAudio(elementId) {
+    var audio = document.getElementById(elementId);
+    audio.muted = false;
+}
+
+function setVolume(elementId, volumeLevel) {
+    var audio = document.getElementById(elementId);
+    audio.volume = volumeLevel;
+}
+
+function getVolume(elementId) {
+    var audio = document.getElementById(elementId);
+    return audio.volume;
+}
