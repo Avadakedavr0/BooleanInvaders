7f30492d8bfc4241fa3cbda5b0d707885316169c
Ävadakedavr0 <dylandd4@hotmail.be>
2024-02-13 11:52:13 +0100

    moved script in index.html to seperate js file

---
+++ BooleanInvadersApp/Client/wwwroot/app.js
@@ -0,0 +1,24 @@
+﻿
+    function changeCursorStyle(style) {
+        document.body.style.cursor = style;
+        }
+
+    window.blazorLocalStorage = {
+        getItem: function (key) {
+                return localStorage.getItem(key);
+            },
+    setItem: function (key, value) {
+        localStorage.setItem(key, value);
+            }
+        };
+
+    function focusGameContainer() {
+            var gameContainer = document.querySelector('.game-container');
+    if (gameContainer) {
+        gameContainer.focus();
+            }
+        };
+    function playHitSound() {
+            var hitSound = document.getElementById('hitSound');
+    hitSound.play();
+        }
